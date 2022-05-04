d866759178706785794dfbe4b5138a90e0355df7
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-04 14:10:09 +0100

    code: add streams component and wire up semantic ui cdn

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
   return (
     <div className="App">
-      App Here
+      <BrowserRouter>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/show" exact component={StreamShow} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+      </BrowserRouter>
     </div>
   );
 }

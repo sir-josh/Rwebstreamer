1f7e03de39029b4b4d72f862f7a56b30d0fdb70f
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-04 19:55:04 +0100

    code: add google auth component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
   return (
@@ -9,6 +10,9 @@ const Header = () => {
         <div className='right menu'>
             <Link to="/" className='item'>All Streams</Link>
             <Link to="/login" className='item'>Login</Link>
+            <div className="item">
+                <GoogleAuth />
+            </div>
         </div>
     </div>
   );

f4665a8c9b3a4649ce8a96a356171f15083a422e
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-06 00:10:57 +0100

    code: automatic handling events in redux form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
+  renderInput({ input }){
+    return <input {...input} />
+  }
 
   render() {
     return (
       <form>
-        <Field name='title' />
-        <Field name='description' />
+        <Field name='title' component={this.renderInput} />
+        <Field name='description' component={this.renderInput} />
       </form>
     )
   }

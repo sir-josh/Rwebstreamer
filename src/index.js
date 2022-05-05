1b29e117fee45211e3d6adef27104b93bd99af4d
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-05 23:02:15 +0100

    code: make project to interact with redux dev tool extension

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

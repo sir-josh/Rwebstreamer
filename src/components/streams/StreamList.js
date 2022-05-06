a77f72ead3ea8ecb6c9f52d668cbfcb022109d8c
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-06 22:42:48 +0100

    code: rendering all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,11 +7,32 @@ class StreamList extends Component {
     this.props.fetchAllStreams();
   }
 
+  displayStreamList(){
+    return this.props.streams.map(stream => {
+      return (
+        <div className='item' key={stream.id} style={{ padding: "12px 0"}}>
+          <i className='large middle aligned icon camera' />
+          <div className='content'>
+            <b>{stream.title}</b>
+            <div className='description' style={{ marginTop: "5px"}}>{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>StreamList</div>
-    )
+      <div>
+        <h2>Streams</h2>
+        <div className='ui celled list'>{ this.displayStreamList() }</div>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchAllStreams })(StreamList);
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) }
+}
+
+export default connect(mapStateToProps, { fetchAllStreams })(StreamList);

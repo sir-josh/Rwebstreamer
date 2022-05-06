973a9c7b6c2c3f5a05e0a6a922a49828f2ef7af5
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-06 19:59:41 +0100

    code: create all action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,8 @@ import {
     EDIT_STREAM
  } from './types';
 
+
+
 export const signIn = (userId) => {
     return {
         type: SIGN_IN,
@@ -26,4 +28,28 @@ export const createStream = formValues => async dispatch => {
     const response = await streams.post('/streams', formValues);
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
+}
+
+export const fetchAllStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+}
+
+export const fetchStream = id => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+}
+
+export const deleteStream = id => async dispatch => {
+    await streams.delelte(`/streams/${id}`);
+
+    dispatch({ type: DELETE_STREAM, payload: id });
 }

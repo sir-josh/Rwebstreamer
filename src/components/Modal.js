44fa8a3b5147208837331d27eb8d22359a2b5000
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-08 17:16:20 +0100

    code: make modal reuseable

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { createPortal } from 'react-dom';
 import history from '../history';
 
-const Modal = () => {
+const Modal = props => {
   return createPortal(
       <div onClick={() => history.push('/')} className='ui dimmer modals visible active' >
           <div onClick={(e) => e.stopPropagation()} className='ui standard modal visible active'>
-              <div className='header'>Delete Stream</div>
+              <div className='header'>{ props.title }</div>
               <div className='content'>
-                  <p>Are sure you want to delete this stream?</p>
+                  <p>{ props.content }</p>
               </div>
-              <div className='actions'>
-                  <div className='ui button black'>Delete</div>
-                  <div className='ui cancel button'>Cancel</div>
-              </div>
+              <div className='actions'>{props.actions} </div>
           </div>
       </div>
       , document.querySelector('#modal'));
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Modal from '../Modal';
 
 function StreamDelete() {
+  const actions = (
+    <div>
+      <button className='ui button black'>Delete</button>
+      <button className='ui cancel button'>Cancel</button>
+    </div>
+  );
+
   return (
     <div>
-      StreamDelete
-      <Modal />
+      <Modal 
+        title="Delete Stream"
+        content="Are sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   )
 }

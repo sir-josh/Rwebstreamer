80fccb4e92ab501802fc7cab068b60bc30da86a1
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-09 14:52:53 +0100

    code: add Switch Router to show one screen component at a time

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router , Route } from "react-router-dom";
+import { Router , Route, Switch } from "react-router-dom";
 
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
@@ -15,11 +15,13 @@ const App = () => {
       <Router history={history}>
         <Header />
         <div className='ui container'>
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/:id" exact component={StreamShow} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          </Switch>
         </div>
       </Router>
     </div>

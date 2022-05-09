cd38aa1774752c9799b06b3060e1e695af46ec92
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-09 09:39:30 +0100

    refactor: change streamDelete from function to class based component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,25 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Modal from '../Modal';
 import history from '../../history';
 
-function StreamDelete() {
-  const actions = (
-    <Fragment>
-      <button className='ui button black'>Delete</button>
-      <button className='ui cancel button'>Cancel</button>
-    </Fragment>
-  );
+class StreamDelete extends Component {
 
-  return (
-    <div>
-      <Modal 
-        title="Delete Stream"
-        content="Are sure you want to delete this stream?"
-        actions={actions}
-        onDismiss ={() => history.push('/')}
-      />
-    </div>
-  )
+  showActionButtons(){
+    return (
+      <Fragment>
+        <button className='ui button black'>Delete</button>
+        <button className='ui cancel button'>Cancel</button>
+      </Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal 
+          title="Delete Stream"
+          content="Are sure you want to delete this stream?"
+          actions={this.showActionButtons()}
+          onDismiss ={() => history.push('/')}
+        />
+      </div>
+    );
+  }
 }
 
 export default StreamDelete;
